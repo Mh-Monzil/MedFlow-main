410271ad3314e8c7d759a41e6cd814e86a0f8534
Anas Ahammad <anasahammad2002@gmail.com>
2024-07-12 17:50:49 +0600

    basic server added

--- medflow-server/index.js
+++ medflow-server/index.js
@@ -1,2 +1,17 @@
-"server"
-"dev monzil"
+const express = require("express")
+
+const app = express()
+const port = process.env.PORT || 3000;
+const cors = require('cors')
+
+require('dotenv').config()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', async(req, res)=>{
+    console.log("Medflow is running")
+})
+app.listen(port, ()=>{
+    console.log(`The server is running from ${port}`)
+})
