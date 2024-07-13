529c2b8814b0ba8cce04c481a86afe3ca1bbc532
Mh-Monzil <monzil246@gmail.com>
2024-07-13 17:42:29 +0600

    add: private route, fix: auth provider

--- medflow-client/src/Routes/Routes.jsx
+++ medflow-client/src/Routes/Routes.jsx
@@ -8,42 +8,46 @@ import SignUp from "@/pages/SignUp";
 import SignIn from "@/pages/SignIn";
 // import Appointment from "@/pages/Appointment";
 import Appointment from "@/pages/Appointment";
+import PrivateRoute from "@/privateRoute/PrivateRoute";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-        {
-          path: "/about",
-          element: <About />,
-        },
-        {
-          path: "/contact",
-          element: <Contact />,
-        },
-        
-        {
-          path: "/services",
-          element: <Services />,
-        },
-        {
-          path: '/signup',
-          element: <SignUp/>
-        },
-        {
-          path: '/signin',
-          element: <SignIn/>
-        }
-        ,
-        {
-          path: "/appointment",
-          element: <Appointment />,
-        },
-      ],
-    },
-  ]);
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+
+      {
+        path: "/services",
+        element: <Services />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        path: "/appointment",
+        element: (
+          <PrivateRoute>
+            <Appointment />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+]);
